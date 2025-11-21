1aa33eb5cb3f3b25b715ebe50b20c829e123f3a3
Johnrick Rabara <158558094+Jrabara101@users.noreply.github.com>
2025-11-21 11:06:34 +0800

    Day 9

---
+++ HTML/Day 9/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('btn-html5')?.addEventListener('click', function() {
+    alert('HTML5 Button clicked!');
+  });
+  document.getElementById('btn-xhtml')?.addEventListener('click', function() {
+    alert('XHTML Button clicked!');
+  });
+  document.getElementById('btn-layout')?.addEventListener('click', function() {
+    alert('CSS Layout Cookbook Button clicked!');
+  });
+  document.getElementById('btn-text')?.addEventListener('click', function() {
+    alert('CSS Styling Text Button clicked!');
+  });
+});
